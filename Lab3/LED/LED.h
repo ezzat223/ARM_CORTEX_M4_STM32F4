66ea190c18a70075b0773439384aacd8ff6c0beb
Ezzat Hegazy <ezzat22hegazy@gmail.com>
2023-06-15 16:12:54 +0300

    Update LED.h

--- Lab3/LED/LED.h
+++ Lab3/LED/LED.h
@@ -3,11 +3,10 @@
 /* Date     : 13 April 2023												   	  */
 /* Version  : V01	                                        				  */
 /******************************************************************************/
-#include"STD_TYPES.h"
-
 #ifndef LED_H
 #define LED_H
 
+#include"STD_TYPES.h"
 
 void LED_voidOn(uint8 Copy_uint8Port, uint8 Copy_uint8PinNum);
 void LED_voidOff(uint8 Copy_uint8Port, uint8 Copy_uint8PinNum);
